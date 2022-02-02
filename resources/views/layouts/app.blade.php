b856b503852cd6118bcc467d005b4bcd20605be7
GinoGreen <luigiverde97@gmail.com>
2022-02-02 17:46:40 +0100

    if auth elenco post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,11 @@
                 <a class="navbar-brand" href="{{ route('admin.index') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a class="navbar-brand" href="{{ route('admin.post.index') }}">
-                    Elenco post
-                </a>
+                @auth
+                    <a class="navbar-brand" href="{{ route('admin.post.index') }}">
+                        Elenco post
+                    </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
