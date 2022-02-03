61f38eb6a0f20b69e2152a1dccca811740bbf26d
GinoGreen <luigiverde97@gmail.com>
2022-02-03 17:24:52 +0100

    error message form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -24,10 +24,15 @@
                value="{{ old('title') }}"
                class="form-control
                   @error('title')
-                     
+                     is-invalid
                   @enderror
                "
             >
+            @error('title')   
+               <p class="@error('title')
+                  invalid-feedback
+               @enderror">{{ $message }}</p>
+            @enderror
          </div>
          <div class="mb-3 form-check">
             <label class="form-check-label" for="content">Contenuto</label>
@@ -38,11 +43,16 @@
                placeholder="Inserisci il contentuto del post"
                id="content"
                class="form-control
-                  @error('')
-                     
+                  @error('content')
+                     is-invalid
                   @enderror
                "
             >{{ old('content') }}</textarea>
+            @error('content')   
+               <p class="@error('content')
+                  invalid-feedback
+               @enderror">{{ $message }}</p>
+            @enderror
          </div>
          <button type="submit" class="btn btn-primary">Submit</button>
          <button type="reset" class="btn btn-secondary">Reset</button>
